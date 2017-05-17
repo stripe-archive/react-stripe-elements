7c359dae770bc14cd5256e991a7f203112dffc09
David Keene <dave@everystory.us>
2017-05-17 09:49:46 -0700

    removing className
    relying on .StripeElement css

--- src/components/Element.js
+++ src/components/Element.js
@@ -21,7 +21,6 @@ const Element = (type: string, hocOptions: {sourceType?: string} = {}) => class 
     onBlur: PropTypes.func,
     onFocus: PropTypes.func,
     onReady: PropTypes.func,
-    className: PropTypes.string,
   }
   static defaultProps = {
     elementRef: noop,
@@ -100,7 +99,7 @@ const Element = (type: string, hocOptions: {sourceType?: string} = {}) => class 
   }
   render() {
     return (
-      <div ref={this.handleRef} className={this.props.className} />
+      <div ref={this.handleRef}  />
     );
   }
 };
