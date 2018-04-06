85eb417718408c54261bc6f0b7bc5715ab31c539
michelle <michellebu@berkeley.edu>
2018-04-06 11:16:41 -0700

    Upgrade Flow to 0.69.0 (#195)

--- demo/async/main.js
+++ demo/async/main.js
@@ -5,4 +5,11 @@ import {render} from 'react-dom';
 
 import {App} from './async';
 
-render(<App />, document.querySelector('.App'));
+const appElement = document.querySelector('.App');
+if (appElement) {
+  render(<App />, appElement);
+} else {
+  console.error(
+    'We could not find an HTML element with a class name of "App" in the DOM. Please make sure you copy index.html as well for this demo to work.'
+  );
+}
--- demo/demo/index.js
+++ demo/demo/index.js
@@ -249,4 +249,12 @@ const App = () => {
     </StripeProvider>
   );
 };
-render(<App />, document.querySelector('.App'));
+
+const appElement = document.querySelector('.App');
+if (appElement) {
+  render(<App />, appElement);
+} else {
+  console.error(
+    'We could not find an HTML element with a class name of "App" in the DOM. Please make sure you copy index.html as well for this demo to work.'
+  );
+}
