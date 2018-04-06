59e22282e5796457234cc743b7c75ab16201fee7
Michelle Bu <michelle@michellebu.com>
2018-04-06 10:39:40 -0700

    Add a warning

--- demo/demo/index.js
+++ demo/demo/index.js
@@ -253,4 +253,8 @@ const App = () => {
 const appElement = document.querySelector('.App');
 if (appElement) {
   render(<App />, appElement);
+} else {
+  console.error(
+    'We could not find an HTML element with a class name of "App" in the DOM. Please make sure you copy index.html as well for this demo to work.'
+  );
 }
