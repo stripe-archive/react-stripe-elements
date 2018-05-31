13e5ca5bb091a7bf9969921d0bc0f8807c751d56
Atty Eleti <31416034+atty-stripe@users.noreply.github.com>
2018-05-31 15:04:04 -0700

    Add IBAN and iDEAL Bank Elements (#209)
    * Add IBAN and iDEAL Bank Elements
    
    * Run prettier on demos
    
    * Add IBAN and iDEAL Bank Elements to demo
    
    * Update gitignore
    
    * Add IBAN and iDEAL Bank Elements to README

--- src/index.js
+++ src/index.js
@@ -5,10 +5,18 @@ import Elements from './components/Elements';
 import Element from './components/Element';
 import PaymentRequestButtonElement from './components/PaymentRequestButtonElement';
 
+// Define Elements, and register their implied token / source types for
+// automatic token / source creation.
+
+// Card
 const CardElement = Element('card', {
   impliedTokenType: 'card',
   impliedSourceType: 'card',
 });
+
+// Split Fields
+// Note: we only register the CardNumberElement for split fields so that we have
+// a unique Element to infer when calling `wrappedCreateToken` or `wrappedCreateSource`.
 const CardNumberElement = Element('cardNumber', {
   impliedTokenType: 'card',
   impliedSourceType: 'card',
@@ -16,6 +24,15 @@ const CardNumberElement = Element('cardNumber', {
 const CardExpiryElement = Element('cardExpiry');
 const CardCVCElement = Element('cardCvc');
 const PostalCodeElement = Element('postalCode');
+
+// IBAN
+const IbanElement = Element('iban', {
+  impliedTokenType: 'bank_account',
+  impliedSourceType: 'sepa_debit',
+});
+
+// iDEAL Bank
+const IdealBankElement = Element('idealBank', {impliedSourceType: 'ideal'});
 
 export {
   StripeProvider,
@@ -27,4 +44,6 @@ export {
   CardCVCElement,
   PostalCodeElement,
   PaymentRequestButtonElement,
+  IbanElement,
+  IdealBankElement,
 };
