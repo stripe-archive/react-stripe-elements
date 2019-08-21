f7f2b6f250197b5882043bc3ddd191e36966a1ab
Christian Weiss <cweiss@stripe.com>
2019-08-21 16:29:12 -0400

    upgrade react

--- test/setupJest.js
+++ test/setupJest.js
@@ -1,6 +1,6 @@
 // @noflow
 
 import {configure} from 'enzyme';
-import Adapter from 'enzyme-adapter-react-15';
+import Adapter from 'enzyme-adapter-react-16';
 
 configure({adapter: new Adapter()});
